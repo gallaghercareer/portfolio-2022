be3c8cae3fd3c6629417b1b2cec68201d13f3ade
John Gallagher <gallaghercareer@gmail.com>
2025-03-07 09:38:17 -0500

    Updated 2025
    Updated for github pages deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import Home from './Components/Home'
 import Weather from './Components/Weather'
-import {BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from './Components/Footer'
 import Navbar from './Components/Navbar'
 import ShoppingCart from './Components/ShoppingCart'
